75954d7cb146c4b385fdbbdde6983eb12123a9d0
Marcelkibor <kibormarcel90@gmail.com>
2023-02-22 11:04:14 +0300

    DAP-8-Login-Functionality

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import swal from 'sweetalert';
 
 const Home = () => {
+  const checkUserSession = localStorage.getItem("authenticated");
+if(checkUserSession !== "true"){
+  window.location.href = "/login"
+}
+useEffect(()=>{
+const banner = localStorage.getItem("Banner")
+  if(banner!==null){
+    swal({
+      title:'Success!',
+      text:"Logged In",
+      icon:"success",
+  })
+  }
+localStorage.removeItem("Banner")
+},[])
   return (
     <div>Home</div>
   )
