c7652a768bef2e02a6c96d81c81dc724a6d8e878
Marcelkibor <kibormarcel90@gmail.com>
2023-02-27 13:36:43 +0300

    DAP-8 Refactor Authentication Tests

--- src/authentication/Login/Login.test.tsx
+++ src/authentication/Login/Login.test.tsx
@@ -22,6 +22,7 @@ beforeEach(()=>{
   userNameField = getByPlaceholderText(/Username/i);
   passwordField = getByPlaceholderText(/Password/i);
   loginButton = getByRole("button",{name:"Log In"});
+
 })
 
 afterEach(()=>{
@@ -35,6 +36,11 @@ afterAll(()=>{
 
 describe("Login",()=>{
 
+const user = {
+  username:'Admin',
+  password: 'Admin123'
+}
+
 it("Should render the form inputs and submit button",()=>{
   expect(userNameField).toBeTruthy()
   expect(passwordField).toBeTruthy()
@@ -42,22 +48,27 @@ it("Should render the form inputs and submit button",()=>{
 });
 
 it("Should change form values when a user types a new entry",()=>{
-  fireEvent.change(userNameField,{target:{value:"MaryJane"}})
-  fireEvent.change(passwordField,{target:{value:"MJ123"}})
-  expect((userNameField as HTMLInputElement).value).toBe("MaryJane");
-  expect((passwordField as HTMLInputElement).value).toBe("MJ123");
+  fireEvent.change(userNameField,{target:{value:user.username}})
+  fireEvent.change(passwordField,{target:{value:user.password}})
+  expect((userNameField as HTMLInputElement).value).toBe(user.username);
+  expect((passwordField as HTMLInputElement).value).toBe(user.password);
 });
 
-it("Should display an error message when any form input is empty on submit", async()=>{
+it("Should display an error message when form inputs are empty on submit", async()=>{
     await act(async ()=>{
       const errorMessage1 = await LoginRequest("","")
-      const errorMessage2 = await LoginRequest("Admin","")
-      const errorMessage3 = await LoginRequest("","Admin123")
+      const errorMessage2 = await LoginRequest(user.username,"")
+      const errorMessage3 = await LoginRequest("",user.password)
       expect(errorMessage1).toBe("Please fill in the form")
       expect(errorMessage2).toBe("Please fill in the form")
       expect(errorMessage3).toBe("Please fill in the form")
     })
 });
+
+it("Should not display an empty form error when inputs contain some value",async()=>{
+  const errorMessage = await LoginRequest(user.username,user.password)
+  expect(errorMessage).not.toBe("Please fill in the form")
+})
 
 it("Should not authenticate a user with invalid credentials",async()=>{
 const response = await fetch('https://dev3.openmrs.org/openmrs/ws/rest/v1/session', {
@@ -73,11 +84,11 @@ expect(responseBody.authenticated).toBe(false)
 it("Should authenticate a user with valid credentials",async()=>{
 const response = await fetch('https://dev3.openmrs.org/openmrs/ws/rest/v1/session', {
   headers: {
-    Authorization: 'Basic ' + btoa('Admin:Admin123'),
+    Authorization: 'Basic ' + btoa(`${user.username}:${user.password}`),
   },
 });
 expect(response.status).toEqual(200)
 const responseBody = await response.json()
 expect(responseBody.authenticated).toBe(true)
-})
-})
+})}
+)
